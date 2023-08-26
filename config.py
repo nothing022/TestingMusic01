53cf7323d906421a01707c98434c994b92d045a3
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-08-27 00:34:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5339377884").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/nothing022/NothingMusic01")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/nothing022/TestingMusic01")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
