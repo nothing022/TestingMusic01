f273b94c5c5b40eb5564fe09a02d6a99be5d2b64
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-08-27 14:16:15 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -79,7 +79,7 @@ async def gen_thumb(videoid, user_id):
         d = np.array(a)
         e = np.dstack((c, d))
         f = Image.fromarray(e)
-        x = f.resize((200, 200))
+        x = f.resize((190, 190))
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
         bg = Image.open(f"AnonX/assets/anonx.png")
